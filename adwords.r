b6d58542d9eab508058c6aa06973ea830608acaa
Chris Salvato <csalvato@gmail.com>
2016-01-12 15:02:53 -0500

    Add second query for the hell of it

--- adwords.r
+++ adwords.r
@@ -5,6 +5,20 @@ pgsql <- JDBC("org.postgresql.Driver", "../database_drivers/postgresql-9.2-1004.
 #heroku_db <- dbConnect(pgsql, "jdbc:postgresql://ec2-54-221-203-136.compute-1.amazonaws.com:5502/dfh97e63ls7ag8?user=u1gg5j81iss15&password=p1g2km19noav948l6q7net768vu&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")
 datawarehouse_db <- dbConnect(pgsql, "jdbc:postgresql://127.0.0.1:5438/mps_oltp?user=oltp_reader&password=0Ltpr33@donly")
 
+# Notes from John Wooley:
+# This query should give real-time feedback, but could change over the course of the week
+# if the users make changes.  This could be run on the Heroku follower if the
+# dim_users table was removed.  I included it because there is useful info in it (or will
+# be on Friday - those fields are updated after deliveries are finalized).
+
+# Kevin says that a transaction record is created immediately for new orders and
+# updated with any changes through to Thursday night.
+
+# Recurring orders are created on Tuesday and updated until Thursday night.
+
+# Transaction values can be a bit confusing, because they include system credits
+# and discounts.  The price field is generally == list price of the meal plan, but there
+# are situations where it can get nuked for older data.
 query <- "select
     t.created_at
 , du.name
@@ -41,10 +55,39 @@ left outer join
 discounts d on ti.discount_id = d.id
 order by 1 desc"
 db_transactions <- dbGetQuery(datawarehouse_db, query)
+
+# Notes from John: 
+# The data from this query will be updated nightly.  
+# The numbers for upcoming week will not be solid until Friday morning.
+
+# This will not catch gift cards and will include cancelled orders 
+# unless you uncomment the where clause. 
+
+# You can look at the available field and decide what you might want. 
+# The price field is list_price, which is not 100% accurate but should  
+# be reasonable for recent data.  The transaction_amount with fluctutate some 
+# if there are refunds.   
+
+# Week_start is Monday of the week the orders meals would be delivered in.
+# It is not the same as the created_at field above, which 
+# is a record creation timestamp. 
+
+query <- "select 
+ * 
+from 
+  fact_orders 
+    inner join 
+  vw_stage_mixpanel_orders vw on fact_orders.user_id = vw.user_id 
+    inner join 
+  dim_users du on fact_orders.user_id = du.user_id 
+-- where 
+--   not fo.cancelled"
+unknown_data <- dbGetQuery(datawarehouse_db, query)
+  
 # dbDisconnect(heroku_db)
 dbDisconnect(datawarehouse_db)
 
-# Collapse
+# Create a table showing total value by campaign
 
 ########################
 
