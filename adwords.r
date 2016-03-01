ae8c1cf057480024b31eb63c0b8b45f7e4d08cec
Chris Salvato <csalvato@gmail.com>
2016-03-01 14:47:26 -0500

    add adwords query from DW

--- adwords.r
+++ adwords.r
@@ -26,37 +26,7 @@ pgsql <- JDBC("org.postgresql.Driver", "../database_drivers/postgresql-9.2-1004.
 #heroku_db <- dbConnect(pgsql, "jdbc:postgresql://ec2-54-221-203-136.compute-1.amazonaws.com:5502/dfh97e63ls7ag8?user=u1gg5j81iss15&password=p1g2km19noav948l6q7net768vu&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")
 datawarehouse_db <- dbConnect(pgsql, "jdbc:postgresql://127.0.0.1:5438/mps_oltp?user=oltp_reader&password=0Ltpr33@donly")
 
-# Notes from John Wooley:
-# This query should give real-time feedback, but could change over the course of the week
-# if the users make changes.  This could be run on the Heroku follower if the
-# dim_users table was removed.  I included it because there is useful info in it (or will
-# be on Friday - those fields are updated after deliveries are finalized).
-
-# Kevin says that a transaction record is created immediately for new orders and
-# updated with any changes through to Thursday night.
-
-# Recurring orders are created on Tuesday and updated until Thursday night.
-
-# Transaction values can be a bit confusing, because they include system credits
-# and discounts.  The price field is generally == list price of the meal plan, but there
-# are situations where it can get nuked for older data.
-# query <- qq("
-# select
-#      du.name
-# --    , du.user_id
-#     , du.lifetime_transaction_value
-#     , du.lifetime_orders
-#     , du.first_order_week
-#     , dl.store_front_id
-#     , gsm.*
-# from 
-#   stage.gs_mixpanel gsm
-# inner join
-#   dim_users du on gsm.user_id = du.user_id
-# left outer join
-#   dim_locations dl on dl.location_id = du.loc_primary
-# order by 1")
-query <- qq("
+purchases_query <- qq("
 select
   t.created_at as transaction_date
   , du.name as user_name
@@ -122,44 +92,21 @@ where
 --  t.refunded IS NOT TRUE
 order by t.created_at desc")
 
-#********************************************************************************
-#TODO: This includes the list price, but does not subtract out a discount 
-#      (e.g. first order discount, credit from previous refun applied).  
-#      Will need to figure out a way to subtract out discounts they receive 
-#      on an order.
-#********************************************************************************
-db_transactions <- dbGetQuery(datawarehouse_db, query)
+adwords_campaigns_query <- qq("
+select
+  *
+from 
+  stage.gs_adwords_campaigns awc
+where 
+  awc.date between '2015-12-17' and '2016-03-01'
+order by awc.date desc")
+
+db_adwords_campaigns <- dbGetQuery(datawarehouse_db, adwords_campaigns_query)
+db_transactions <- dbGetQuery(datawarehouse_db, purchases_query)
+
+View(db_adwords_campaigns)
 View(db_transactions)
 
-# Notes from John: 
-# The data from this query will be updated nightly.  
-# The numbers for upcoming week will not be solid until Friday morning.
-
-# This will not catch gift cards and will include cancelled orders 
-# unless you uncomment the where clause. 
-
-# You can look at the available field and decide what you might want. 
-# The price field is list_price, which is not 100% accurate but should  
-# be reasonable for recent data.  The transaction_amount with fluctutate some 
-# if there are refunds.   
-
-# Week_start is Monday of the week the orders meals would be delivered in.
-# It is not the same as the created_at field above, which 
-# is a record creation timestamp. 
-
-# query <- "select 
-#  * 
-# from 
-#   fact_orders 
-#     inner join 
-#   stage.gs_mixpanel vw on fact_orders.user_id = vw.user_id 
-#     inner join 
-#   dim_users du on fact_orders.user_id = du.user_id 
-# -- where 
-# --   not fo.cancelled"
-# unknown_data <- dbGetQuery(datawarehouse_db, query)
-  
-# dbDisconnect(heroku_db)
 dbDisconnect(datawarehouse_db)
 
 # Make sure campaign IDs are integers for future joins
