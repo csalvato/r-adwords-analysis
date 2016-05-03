d7f3301b1b8a9d6eecf5be97084585ffab4e1b89
Chris Salvato <csalvato@gmail.com>
2016-05-03 18:35:42 -0400

    Create experimental means of gathering data from mixpanel.

--- mixpanel_test.r
+++ mixpanel_test.r
@@ -5,8 +5,18 @@ account = mixpanelCreateAccount("Power Supply - Main",
                                 secret="af3b32cc21c7b6e91b71f7c0417735d2", 
                                 key="ce370ab09a166e168d448080b55715f6")
 
-events <- mixpanelGetEvents(account, 
+events_aw <- mixpanelGetEvents(account, 
                   from=20151217,
-                  to=20160421,
+                  to=20160503,
                   event=array("Completed Order"),
                   where='(properties["latest_ad_search"]) and (properties["latest_ad_utm_source"] == "Google")')
+
+events_aw_df <- data.frame(events_aw)
+
+events_bing <- mixpanelGetEvents(account, 
+                               from=20151217,
+                               to=20160503,
+                               event=array("Completed Order"),
+                               where='(properties["latest_ad_search"]) and (properties["latest_ad_utm_source"] == "Bing")')
+
+events_bing_df <- data.frame(events_bing)
