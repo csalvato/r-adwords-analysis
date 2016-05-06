ca4c0dab291d8935cc579760da8bf027b9e11bc1
Chris Salvato <csalvato@gmail.com>
2016-05-06 10:53:01 -0400

    Change values to use heroku DB instead of DW.

--- adwords.r
+++ adwords.r
@@ -171,14 +171,14 @@ adwords_campaigns_data <- getData(clientCustomerId="479-107-0932", google_auth=g
 
 # Retrieve revenue data
 pgsql <- JDBC("org.postgresql.Driver", "../database_drivers/postgresql-9.4.1208.jre6.jar", "`")
-# heroku_db <- dbConnect(pgsql, string_from_file("jdbc_heroku_string.txt"))
+heroku_db <- dbConnect(pgsql, string_from_file("jdbc_heroku_string.txt"))
 datawarehouse_db <- dbConnect(pgsql, string_from_file("jdbc_datawarehouse_string.txt"))
 
 transactions_query <- string_from_file("mixpanel_transactions_query.sql")
 influencer_metrics_query <- string_from_file("influencer_metrics_query.sql")
 
 db_influencer_metrics <- dbGetQuery(datawarehouse_db, influencer_metrics_query)
-db_transactions <- dbGetQuery(datawarehouse_db, transactions_query)
+db_transactions <- dbGetQuery(heroku_db, transactions_query)
 
 # Join Mixpanel Conversion Data with Data Warehouse transaction data
 mixpanel_adwords_conversions <- data.frame(mixpanel_adwords_conversions)
--- mixpanel_transactions_query.sql
+++ mixpanel_transactions_query.sql
@@ -1,7 +1,7 @@
 "select
   t.created_at as transaction_date
-  , du.name as user_name
-  , du.user_id as app_user_id
+  , u.name as user_name
+  , u.id as app_user_id
   , t.discount_amount
   , ti.credit_used
   , t.sales_tax_amount
@@ -39,7 +39,7 @@
   left outer join
   discounts d on ti.discount_id = d.id
   inner join
-  dim_users du on t.user_id = du.user_id
+  users u on t.user_id = u.id
 where
   t.created_at between '@{start_date}' and '@{end_date}'
 order by t.created_at desc"
