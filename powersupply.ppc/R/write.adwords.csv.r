f17e5a56b313f074161248d2c6018820038b37e0
Chris Salvato <csalvato@gmail.com>
2016-05-08 03:46:26 -0400

    Add Write Excel CSV documentation

--- powersupply.ppc/R/write.adwords.csv.r
+++ powersupply.ppc/R/write.adwords.csv.r
@@ -1,4 +1,14 @@
-write.adwords.csv <- function(data_frame, file){
+#' Write Excel CSV
+#'
+#' Wrapper function to write data frame to an excel-readable CSV file.
+#'
+#' @param data_frame Data frame to be written to CSV file.
+#' @param file The path to the file to be written
+#' @export
+#' @examples
+#' write.excel.csv(data_frame, "some_csv_file.csv")
+
+write.excel.csv <- function(data_frame, file){
   write.csv( data_frame,
              file=file,
              eol = "\r\n", 
