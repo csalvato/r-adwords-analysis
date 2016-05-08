763bd9ab85ae62877ffd15a72217ef55e5c6f494
Chris Salvato <csalvato@gmail.com>
2016-05-08 01:28:51 -0400

    Add as.week and date_filter documentation and tests.

--- powersupply.ppc/R/string_from_file.r
+++ powersupply.ppc/R/string_from_file.r
@@ -1,3 +1,17 @@
-string_from_file <- function(file_name){
-  GetoptLong::qq(scan(file_name, character()))
+#' String From File
+#'
+#' Reads a string literal from a file and performs string
+#' interpolation on values from the string using `@{}` syntax. 
+#' (e.g @{start_date})
+#'
+#' Should consider using Whisker... https://github.com/edwindj/whisker
+#' @param file_path Path to the file containing the string.
+#' @param ... Array of variables to be interpolated in the string.
+#' @export
+#' @examples
+#' string_from_file('./transactions_query.sql')
+library(GetoptLong)
+
+string_from_file <- function(file_path){
+  GetoptLong::qq(scan(file_path, character()))
 }
