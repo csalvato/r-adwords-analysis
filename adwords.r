e6723b245214d2c755cdc07de528b24eaa1365c8
Chris Salvato <csalvato@gmail.com>
2016-05-25 20:46:45 -0400

    Replace user overview logic with user overview function.

--- adwords.r
+++ adwords.r
@@ -67,20 +67,7 @@ device_overview <- keywords_elog %>%
                     arrange(desc(earnings))
 
 # Note, number of transactions is NOT the same as number of orders
-user_overview <- keywords_elog %>% 
-                  filter(!is.na(user_id)) %>% #Remove NA user_ids (which means they are not monetary transactions)
-                  group_by(user_id) %>%
-                  summarize(name = first(user_name), 
-                            num_transactions=length(date), 
-                            earnings = sum(money_in_the_bank_paid_to_us, na.rm=TRUE),
-                            contribution = earnings*.25,
-                            campaign_name = first(campaign_name),
-                            keyword=first(keyword),
-                            referred_users=sum(new_referred_users, na.rm=TRUE),
-                            referred_earnings=sum(referred_users_transaction_amount,na.rm=TRUE),
-                            referred_contribution = referred_earnings * .25,
-                            total_earnings = earnings + referred_earnings,
-                            total_contribution = contribution + referred_contribution)
+adwords_user_overview <- user_overview(adwords_keywords_elog)
 
 consolidated_user_overview <- user_overview %>% select(name,
                                                         keyword, 
@@ -168,20 +155,7 @@ device_overview <- keywords_elog %>%
   arrange(desc(earnings))
 
 # Note, number of transactions is NOT the same as number of orders
-user_overview <- keywords_elog %>% 
-  filter(!is.na(user_id)) %>% #Remove NA user_ids (which means they are not monetary transactions)
-  group_by(user_id) %>%
-  summarize(name = first(user_name), 
-            num_transactions=length(date), 
-            earnings = sum(money_in_the_bank_paid_to_us, na.rm=TRUE),
-            contribution = earnings*.25,
-            campaign_name = first(campaign_name),
-            keyword=first(keyword),
-            referred_users=sum(new_referred_users, na.rm=TRUE),
-            referred_earnings=sum(referred_users_transaction_amount,na.rm=TRUE),
-            referred_contribution = referred_earnings * .25,
-            total_earnings = earnings + referred_earnings,
-            total_contribution = contribution + referred_contribution)
+bing_user_overview <- user_overview(bing_keywords_elog, view=TRUE)
 
 consolidated_user_overview <- user_overview %>% select(name,
                                                        keyword, 
