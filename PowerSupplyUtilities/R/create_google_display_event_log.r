d01628c22cf791d352921d9d1573b333157be2a9
Chris Salvato <csalvato@gmail.com>
2016-06-06 21:29:48 -0400

    Clean up the GDN event log generation script and re-incorporate referral metrics

--- PowerSupplyUtilities/R/create_google_display_event_log.r
+++ PowerSupplyUtilities/R/create_google_display_event_log.r
@@ -33,7 +33,7 @@ create_google_display_event_log <- function(from=Sys.Date(),
   #Filter out people where their first order was not in the specified start date and end date
   db_transactions <- db_transactions %>% filter(is.element(app_user_id, db_first_transactions$id))
 
-  db_transactions <- clean_transactions_data(db_transactions)
+  db_transactions <- clean_transactions_data_for_adwords(db_transactions)
 
   # Add campaign names to db_transactions log
   db_transactions <- campaign_lookup_table(from=from, to=to) %>%
@@ -46,26 +46,27 @@ create_google_display_event_log <- function(from=Sys.Date(),
   keywords_elog$week <- as.week(keywords_elog$date)
   keywords_elog <- keywords_elog %>% arrange(week)
 
-  # TODO: Implement referral metrics for GDN analysis.
   # Create join table for user_id and the keyword and campaign_name of first purchase
-  # user_first_acquisition_metrics <- keywords_elog %>%
-  #                                   filter(!is.na(user_id)) %>% #Remove NA user_ids (which means they are not monetary transactions)
-  #                                   group_by(user_id) %>%
-  #                                   summarize(keyword = first(keyword),
-  #                                             campaign_name=first(campaign_name),
-  #                                             campaign_id=first(campaign_id),
-  #                                             device=first(device),
-  #                                             match_type=first(match_type))
+  user_first_acquisition_metrics <- keywords_elog %>%
+                                    filter(!is.na(user_id)) %>% #Remove NA user_ids (which means they are not monetary transactions)
+                                    group_by(user_id) %>%
+                                    summarize(keyword = first(keyword),
+                                              campaign_name=first(campaign_name),
+                                              campaign_id=first(campaign_id),
+                                              device=first(device),
+                                              match_type=first(match_type))
 
   #Add influencer metrics to the event log
-  #db_influencer_metrics <- get_referrals_data(from=from, to=to)
-  #influencer_metrics_with_user_data <- db_influencer_metrics %>%
-                                      #rename(week=week_start,
-                                      #       user_id=influencer_id) %>%
-                                      #mutate(week = as.Date(week, format = '%Y-%m-%d')) %>%
-                                      #inner_join(user_first_acquisition_metrics, by=c(user_id="user_id")) %>%
-                                      #filter(week >= from, week <= to)
-  #keywords_elog <- rbind.fill(keywords_elog, influencer_metrics_with_user_data)
+  db_influencer_metrics <- get_referrals_data(from=from, to=to)
+  influencer_metrics_with_user_data <- db_influencer_metrics %>%
+                                      rename(week=week_start,
+                                            user_id=influencer_id) %>%
+                                      mutate(week = as.Date(week, format = '%Y-%m-%d')) %>%
+                                      inner_join(user_first_acquisition_metrics, by=c(user_id="user_id")) %>%
+                                      filter(week >= from, week <= to)
+  keywords_elog <- rbind.fill(keywords_elog, influencer_metrics_with_user_data)
+
+  keywords_elog <- keywords_elog %>% filter(grepl("Rmktg",campaign_name))
 
   keywords_elog <- keywords_elog %>% 
                     select( -transaction_date,
